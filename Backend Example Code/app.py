78f1e1816ff5def8813279559624e3453e2f1cba
aratakatayama <99364243+aratakatayama@users.noreply.github.com>
2023-10-18 15:22:23 -0400

    removed test codes

--- Backend Example Code/app.py
+++ Backend Example Code/app.py
@@ -29,7 +29,7 @@ def initialize_database():
                 ("Sharon", "sharon@earlham.edu")
                 )
     cur.execute("INSERT INTO users (name, email) VALUES (?, ?)",
-                ("Tien", "kalppsummer@earlham.com")
+                ("Tien", "tien@earlham.com")
                 )
     connection.commit()
     connection.close()
@@ -44,10 +44,6 @@ def get_db_connection():
 # WEB ROUTES
 @app.route("/")
 def index():
-    # # selecting all entries in the users table
-    # users = conn.execute("SELECT * FROM users").fetchall()
-    # # users argument contains result of SQL query 
-    # return render_template("index.html", users=users)
     conn = get_db_connection()
     return render_template("index.html")
 
@@ -58,18 +54,6 @@ def getAll():
     cursor = conn.cursor()
     all_users = conn.execute("SELECT * FROM users").fetchall()
     return all_users
-
-@app.route("/api/search", methods=["GET"])
-def search():
-    conn = sqlite3.connect("user_database.db")
-    cursor = conn.cursor()
-    # If the URL is something like /search?search_name=John, this line will extract "John" as the value for search_name
-    search_name = request.args.get("search_name", "")
-    
-    # select the user information of the searched name
-    search_results = conn.execute("SELECT * FROM users WHERE name LIKE ?", ("%" + search_name + "%",)).fetchall()
-
-    return search_results
 
 if __name__ == "__main__":
     app.run(debug=True)
