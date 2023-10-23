7b081e900a158ffee00589279992a2347575f2ef
aratakatayama <99364243+aratakatayama@users.noreply.github.com>
2023-10-23 16:19:51 -0400

    added comments to code

--- Backend Example Code/app.py
+++ Backend Example Code/app.py
@@ -36,22 +36,28 @@ def initialize_database():
 
 # opens a connection to the user_database.db database
 def get_db_connection():
+    # call the function that initializes the database
     initialize_database()
     conn = sqlite3.connect("user_database.db")
     conn.row_factory = sqlite3.Row
     return conn
 
 # WEB ROUTES
+# when accessing "https:~/" it should open the main frontend page of website
 @app.route("/")
 def index():
     conn = get_db_connection()
+    # return the basis frontend page
     return render_template("index.html")
 
 # API ROUTES
+# when accessing "https:~/api/all" it will show raw data (which is not something you would see as a user!)
+# define methods to be http GET request
 @app.route("/api/all", methods=["GET"])
 def getAll():
     conn = sqlite3.connect("user_database.db")
     cursor = conn.cursor()
+    # using SQL query to retrieve desired information from database
     all_users = conn.execute("SELECT * FROM users").fetchall()
     return all_users
 
