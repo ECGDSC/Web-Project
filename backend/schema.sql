152cc39c5231ac2c0fef3e7555e1ebbbe853e1f3
zKnfz <cayden5404@gmail.com>
2023-10-31 16:29:44 -0400

    Starting backend

---
+++ backend/schema.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    email TEXT NOT NULL
+);
+
+INSERT INTO users (name, email) VALUES ("Arata", "arata@earlham.edu");
+INSERT INTO users (name, email) VALUES ("Sharon", "sharon@earlham.edu");
+INSERT INTO users (name, email) VALUES ("Tien", "tien@earlham.edu");
+INSERT INTO users (name, email) VALUES ("Musa", "musa@earlham.edu");
+INSERT INTO users (name, email) VALUES ("Parsa", "parsa@earlham.edu");
+INSERT INTO users (name, email) VALUES ("Anahit", "anahit@earlham.edu");
+ 
