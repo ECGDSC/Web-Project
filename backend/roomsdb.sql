3775d14fbf020c2330b4b1dd7e1535330dde5f40
amkatayama <113309314+amkatayama@users.noreply.github.com>
2024-05-09 14:09:57 -0400

    Update roomsdb.sql

--- backend/roomsdb.sql
+++ backend/roomsdb.sql
@@ -5,7 +5,30 @@ CREATE TABLE rooms (
     status INTEGER NOT NULL
 );
 
-INSERT INTO rooms(id, status) VALUES (211, 1);
-INSERT INTO rooms(id, status) VALUES (212, 0);
-INSERT INTO rooms(id, status) VALUES (213, 1);
+INSERT INTO rooms(id, status) VALUES (100, 0);
+INSERT INTO rooms(id, status) VALUES (101, 0);
+INSERT INTO rooms(id, status) VALUES (103, 0);
+INSERT INTO rooms(id, status) VALUES (104, 1);
+INSERT INTO rooms(id, status) VALUES (105, 1);
+INSERT INTO rooms(id, status) VALUES (106, 0);
+INSERT INTO rooms(id, status) VALUES (108, 0);
+INSERT INTO rooms(id, status) VALUES (110, 1);
+INSERT INTO rooms(id, status) VALUES (111, 0);
+INSERT INTO rooms(id, status) VALUES (112, 0);
+INSERT INTO rooms(id, status) VALUES (114, 1);
+INSERT INTO rooms(id, status) VALUES (300, 1);
+INSERT INTO rooms(id, status) VALUES (302, 0);
+INSERT INTO rooms(id, status) VALUES (303, 0);
+INSERT INTO rooms(id, status) VALUES (304, 1);
+INSERT INTO rooms(id, status) VALUES (308, 1);
+INSERT INTO rooms(id, status) VALUES (310, 1);
+INSERT INTO rooms(id, status) VALUES (312, 1);
+INSERT INTO rooms(id, status) VALUES (314, 0);
+INSERT INTO rooms(id, status) VALUES (315, 0);
+INSERT INTO rooms(id, status) VALUES (316, 0);
+INSERT INTO rooms(id, status) VALUES (317, 0);
+INSERT INTO rooms(id, status) VALUES (318, 1);
 
+-- INSERT INTO rooms(id, status) VALUES (211, 0);
+-- INSERT INTO rooms(id, status) VALUES (212, 0);
+-- INSERT INTO rooms(id, status) VALUES (213, 1);
